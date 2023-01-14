edb8d52649e34d4819eb0a5ff70242494eb9196e
Philip Chung <philip.f.chung@gmail.com>
2023-01-13 20:40:55 -0800

    add clean_covid_result_value to utils; cleanup create_uuid

--- preop_covid/utils.py
+++ preop_covid/utils.py
@@ -1,27 +1,68 @@
 import hashlib
 
 
-def create_uuid(data: str, format: str = "T-SQL") -> str:
+def create_uuid(data: str, output_format: str = "T-SQL") -> str:
     """Creates unique UUID using BLAKE2b algorithm.
 
     Args:
         data (str): input data used to generate UUID
-        format (str): Output format.
-            `None` results in raw 32-char digest being returned as UUID.
+        output_format (str): Output format.
+            `raw` results in raw 32-char digest being returned as UUID.
             `T-SQL` results in 36-char UUID string (32 hex values, 4 dashes)
                 delimited in the same style as `uniqueidentifier` in T-SQL databases.
 
     Returns:
         Formatted UUID.
     """
-    data = data.encode("UTF-8")
-    digest = hashlib.blake2b(data, digest_size=16).hexdigest()
-    if format is None:
+    data = data.encode("UTF-8")  # type: ignore
+    digest = hashlib.blake2b(data, digest_size=16).hexdigest()  # type: ignore
+    if output_format == "raw":
         return digest
-    elif format == "T-SQL":
-        uuid = (
-            f"{digest[:8]}-{digest[8:12]}-{digest[12:16]}-{digest[16:20]}-{digest[20:]}"
-        )
+    elif output_format == "T-SQL":
+        uuid = f"{digest[:8]}-{digest[8:12]}-{digest[12:16]}-{digest[16:20]}-{digest[20:]}"
         return uuid
     else:
-        raise ValueError(f"Unknown argument {format} specified for `format`.")
+        raise ValueError(f"Unknown argument {output_format} specified for `return_format`.")
+
+
+def clean_covid_result_value(value: str) -> str:
+    """Converts result values into Categorical value Positive, Negative, Unknown."""
+    positive_values = ["Detected", "POSITIVE", "POS", "detected"]
+    negative_values = [
+        "None detected",
+        "NEGATIVE",
+        "NEG",
+        "Negative",
+        "Not detected",
+        "neg",
+        "None Detected",
+        "negative",
+    ]
+    unknown_values = [
+        "Inconclusive",
+        "Duplicate request",
+        "Cancel order changed",
+        "Reorder requested label error",
+        "Wrong test ordered by practitioner",
+        "Canceled by practitioner",
+        "Follow-up testing required. Sample recollection requested.",
+        "Specimen not labeled",
+        "Reorder requested improper tube/sample type",
+        "Data entry correction see updated information",
+        "Reorder requested sample lost",
+        "Reorder requested sample problem",
+        "Wrong test selected by UW laboratory",
+        "Reorder requested. No sample received.",
+        "Follow-up testing required. Refer to other SARS-CoV-2 Qualitative PCR result on specimen with similar collection date and time.",  # noqa:E501
+        "Cancel see detail",
+    ]
+    if value.lower() in [x.lower() for x in positive_values]:
+        return "Positive"
+    elif value.lower() in [x.lower() for x in negative_values]:
+        return "Negative"
+    elif value.lower() in [x.lower() for x in unknown_values]:
+        return "Unknown"
+    else:
+        raise ValueError(
+            f"Unknown value {value} encountered that is not handled by clean_covid_result_value() logic."
+        )
