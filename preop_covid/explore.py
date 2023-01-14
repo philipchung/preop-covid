8a09aa3e20573eed3db5dcef86c4d92cead93a09
Philip Chung <philip.f.chung@gmail.com>
2023-01-13 20:41:14 -0800

    cleanup explore file

--- preop_covid/explore.py
+++ preop_covid/explore.py
@@ -1,12 +1,12 @@
 #%% [markdown]
 # ## Explore dataset
 #%%
-import hashlib
 from datetime import datetime, timedelta
 from pathlib import Path
 
 import pandas as pd
 from pandas.api.types import CategoricalDtype
+from utils import clean_covid_result_value, create_uuid
 
 # Load Data
 project_dir = Path("/Users/chungph/Developer/preop-covid")
@@ -434,73 +434,7 @@ labs_df.AIMS_Value_Text.value_counts().to_frame()
 # Lets clean these values and then get an accurate count
 
 
-def clean_covid_result_value(value: str):
-    positive_values = ["Detected", "POSITIVE", "POS", "detected"]
-    negative_values = [
-        "None detected",
-        "NEGATIVE",
-        "NEG",
-        "Negative",
-        "Not detected",
-        "neg",
-        "None Detected",
-        "negative",
-    ]
-    unknown_values = [
-        "Inconclusive",
-        "Duplicate request",
-        "Cancel order changed",
-        "Reorder requested label error",
-        "Wrong test ordered by practitioner",
-        "Canceled by practitioner",
-        "Follow-up testing required. Sample recollection requested.",
-        "Specimen not labeled",
-        "Reorder requested improper tube/sample type",
-        "Data entry correction see updated information",
-        "Reorder requested sample lost",
-        "Reorder requested sample problem",
-        "Wrong test selected by UW laboratory",
-        "Reorder requested. No sample received.",
-        "Follow-up testing required. Refer to other SARS-CoV-2 Qualitative PCR result on specimen with similar collection date and time.",  # noqa:E501
-        "Cancel see detail",
-    ]
-    if value.lower() in [x.lower() for x in positive_values]:
-        return "Positive"
-    elif value.lower() in [x.lower() for x in negative_values]:
-        return "Negative"
-    elif value.lower() in [x.lower() for x in unknown_values]:
-        return "Unknown"
-    else:
-        raise ValueError(
-            f"Unknown value {value} encountered that is not handled by clean_covid_result_value() logic."
-        )
-
-
-def create_uuid(data: str, format: str = "T-SQL") -> str:
-    """Creates unique UUID using BLAKE2b algorithm.
-
-    Args:
-        data (str): input data used to generate UUID
-        format (str): Output format.
-            `None` results in raw 32-char digest being returned as UUID.
-            `T-SQL` results in 36-char UUID string (32 hex values, 4 dashes)
-                delimited in the same style as `uniqueidentifier` in T-SQL databases.
-
-    Returns:
-        Formatted UUID.
-    """
-    data = data.encode("UTF-8")
-    digest = hashlib.blake2b(data, digest_size=16).hexdigest()
-    if format is None:
-        return digest
-    elif format == "T-SQL":
-        uuid = f"{digest[:8]}-{digest[8:12]}-{digest[12:16]}-{digest[16:20]}-{digest[20:]}"
-        return uuid
-    else:
-        raise ValueError(f"Unknown argument {format} specified for `format`.")
-
-
-def format_labs_df(labs_df: pd.DataFrame):
+def format_labs_df(labs_df: pd.DataFrame) -> pd.DataFrame:
     # Create UUID for each Lab Value based on MPOG Patient ID, Lab Concept ID, Lab DateTime.
     # If there are any duplicated UUIDS, this means the row entry has the same value for all 3 of these.
     labs_df["LabUUID"] = labs_df.apply(
